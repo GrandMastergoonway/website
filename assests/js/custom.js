12afc4750356d23aeb458168bc9f87f31e99c5d2
GrandMastergoonway <168201458+GrandMastergoonway@users.noreply.github.com>
2024-05-04 19:45:33 +0300

    Update custom.js

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -1,13 +1,16 @@
 const closeBtn = document.getElementById('close');
 const modalMenu = document.getElementById('modal');
-const modalBtn = document.querySelector('.modal__button');
 
-setTimeout(()=>{
+setTimeout(() => {
   modalMenu.classList.add('show');
-},1);
+}, 1);
 
-closeBtn.addEventListener('click',()=>{modalMenu.classList.remove('show')});
-modalBtn.addEventListener('click',()=>{modalMenu.classList.remove('show')});
-modalMenu.addEventListener('click',()=>{modalMenu.classList.remove('show');});
+closeBtn.addEventListener('click', () => {
+  modalMenu.classList.remove('show');
+});
+
+modalMenu.addEventListener('click', () => {
+  modalMenu.classList.remove('show');
+});
 
 const countEl = document.getElementById('count');
